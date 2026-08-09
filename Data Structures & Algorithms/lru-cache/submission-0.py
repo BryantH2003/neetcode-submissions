78590505c8ce022b8ddcee86eaded9f17e8dba06
BryantH2003 <88125614+BryantH2003@users.noreply.github.com>
2026-08-09 15:25:34 -0500

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.val = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+
+        prev.next = node
+        nxt.prev = node
+
+        node.next = nxt
+        node.prev = prev
+
+
+    def get(self, key: int) -> int:
+        
+        # If key exists return value | need to move key to end of cache
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+
+        # else return -1
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+
+        # If key exists then update value of key
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        # else add to end of the cache
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        # If cache exceeds capacity then remove LRU key
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+        
+            del self.cache[lru.key]
+        
